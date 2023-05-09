6cea502b35cb23dccd5981529b0f133afa0e6b95
williamfernandessilva <willgsvs@hotmail.com>
2023-05-08 23:33:56 -0300

    quase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PokemonService } from './pokemon.service';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PokemonComponent, PokemonComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [PokemonService],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    PokemonComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-  
 export class AppModule { }
